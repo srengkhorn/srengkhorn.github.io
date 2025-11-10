f262be397806a7dad07e73b261c1778e8a7294ac
Khorn SRENG <khorn.sreng@chipmongbank.com>
2025-11-10 15:45:48 +0700

    remove old ver

--- site/src/jsMain/kotlin/com/binayshaw7777/kotfolio/components/model/WorkExperience.kt
+++
@@ -1,8 +0,0 @@
-package com.binayshaw7777.kotfolio.components.model
-
-data class WorkExperience(
-    val sequenceNumber: String,
-    val role: String,
-    val organization: String,
-    val duration: String
-)
